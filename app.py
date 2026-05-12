cfb1f1318f9fed77cbdc9308631aac500299dab5
fitnesshype46-beep <fitnesshype46@gmail.com>
2026-05-12 19:47:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def home():
     return send_from_directory('.', 'chat.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
